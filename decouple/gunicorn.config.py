80ed08c6578aeb070ec0669bc6e8686231a56bcb
Matthew <matthewpaulh@hotmail.co.uk>
2025-04-27 21:00:39 +0000

    updated module name

--- decouple/gunicorn.config.py
+++ decouple/gunicorn.config.py
@@ -3,7 +3,7 @@
 from multiprocessing import cpu_count
 from os import environ, path
 from sys import path
-from load_secrets import get_secret, create_secret_file, load_secrets_file, SECRETS_FILE_NAME, FILE_PATH
+from secrets import get_secret, create_secret_file, load_secrets_file, SECRETS_FILE_NAME, FILE_PATH
 
 max_workers = cpu_count
 
