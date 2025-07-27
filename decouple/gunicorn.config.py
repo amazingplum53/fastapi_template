607e2ce378a5b4281ef6ea8ebf3753f7010c081a
Matthew <matthewpaulh@hotmail.co.uk>
2025-07-27 11:33:31 +0000

    Fixed APP_ENV with import

--- decouple/gunicorn.config.py
+++ decouple/gunicorn.config.py
@@ -1,6 +1,6 @@
 """Gunicorn WSGI server configuration."""
 from multiprocessing import cpu_count
-from os import environ, path
+import os
 import sys
 
 sys.path.append("/server/decouple/")
@@ -9,13 +9,13 @@ from decouple.secret.keys import get_secret, create_secret_file, load_secrets_fi
 
 max_workers = cpu_count()
 
-bind = '0.0.0.0:' + environ.get('PORT', '8000')
+bind = '0.0.0.0:' + os.environ.get('PORT', '8000')
 
 max_requests = 1000
 
 workers = max_workers
 
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'decouple.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decouple.settings')
 
 APP_ENV = os.getenv("APP_ENV", "dev")
 
@@ -23,8 +23,9 @@ preload_app = True
 
 def on_starting(server):
 
-    if not path.exists(f"{FILE_PATH}/{SECRETS_FILE_NAME}.env"): 
+    if not os.path.exists(f"{FILE_PATH}/{SECRETS_FILE_NAME}.env"): 
         try:
+            print(f"Fetching {APP_ENV} secrets")
             secrets = get_secret(APP_ENV)
             create_secret_file(secrets)
             load_secrets_file()
