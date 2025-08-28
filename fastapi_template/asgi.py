b58831a7c068392f9283a376579a2e72d321835f
Matt <matthewpaulh@hotmail.co.uk>
2025-08-28 15:55:07 +0100

    fixed run command

--- fastapi_template/asgi.py
+++ fastapi_template/asgi.py
@@ -16,9 +16,9 @@ import settings
 
 if __name__ == "__main__":
     uvicorn.run(
-        app, 
+        "main:app", 
         host="0.0.0.0",
-        port="8000",
+        port=8000,
         workers=cpu_count(),
         timeout_keep_alive=5,
         proxy_headers=True,
